0571e66dca57f07ef5fd17a1880852332e497bb3
Анастасия Лузинсан <luzinsan@mail.ru>
2022-04-12 03:49:13 +0700

    задание

--- Lab3Chm/Lab3Chm.cpp
+++ Lab3Chm/Lab3Chm.cpp
@@ -1,9 +1,21 @@
 ﻿#include <iostream>
 #include "Eigenvalue.h"
-
+using namespace luMath;
 int main()
 {
+    setlocale(LC_ALL, "Rus");
+    Eigenvalue<double> data;
 
+    switch (data.getTask())
+    {
+    case Eigenvalue<double>::TASK::EIGENVALUES:
+        data.GetFrobenius();
+        break;
+    case Eigenvalue<double>::TASK::EIGENVECTORS:
+        //data.getDeterminant();
+        break;
+    }
+    
     return 0;
 }
 
